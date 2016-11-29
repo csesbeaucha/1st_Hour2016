41383c388c5ed45a8c7f76356e9bbeaf5cd93224
csesbeaucha <stephenbeauchamp@claytonschools.net>
2016-11-29 08:08:15 -0600

    strategy

--- team0.py
+++ team0.py
@@ -25,8 +25,12 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if my_history=='':
+        return 'b'
+    elif their_history[-1] =='b':
+        return 'b'
+    else:
+        return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -65,4 +69,4 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='c')             
